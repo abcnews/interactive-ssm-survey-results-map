a5c12d83c09d316aface27cae833fa44b4a43581
Nathan Hoad <nathan@nathanhoad.net>
2017-11-15 17:19:44 +1000

    Fixed label text centering

--- src/data/colour-scale.js
+++ src/data/colour-scale.js
@@ -1,26 +1,28 @@
 const Scale = require('d3-scale');
 
 const colours = [
-    '#00a3cf',
-    '#56b2d7',
-    '#7fc1df',
-    '#a2d0e7',
-    '#c2e0ef',
-    '#e1eff7',
-    '#ffffff',
-    '#ffe7dc',
-    '#ffcfb7',
-    '#feb593',
-    '#f99d70',
-    '#f2844f',
-    '#e86b2b'
+  '#00a3cf',
+  '#56b2d7',
+  '#7fc1df',
+  '#a2d0e7',
+  '#c2e0ef',
+  '#e1eff7',
+  '#ffffff',
+  '#ffe7dc',
+  '#ffcfb7',
+  '#feb593',
+  '#f99d70',
+  '#f2844f',
+  '#e86b2b'
 ];
 
 module.exports = () => {
-    let min = 0.2;
-    let max = 0.8;
+  let min = 0.1;
+  let max = 0.9;
 
-    return Scale.scaleQuantize().domain([min, max]).range(colours);
+  return Scale.scaleQuantize()
+    .domain([min, max])
+    .range(colours);
 };
 
 module.exports.colours = colours;
